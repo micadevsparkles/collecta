40b4949bad8b3739aba0aff690f64935c22251ee
micadevsparkles <53092907+micadevsparkles@users.noreply.github.com>
2026-04-25 13:02:17 -0300

    Add project-level plugin configuration in build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Arquivo de configuração de plugins a nível de projeto
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
