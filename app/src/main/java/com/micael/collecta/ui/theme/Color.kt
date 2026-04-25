80bdc8b0e24a205e1b2247bd0c9f1341e035e5af
micadevsparkles <53092907+micadevsparkles@users.noreply.github.com>
2026-04-25 15:07:00 -0300

    Add color definitions for UI theme

---
+++ app/src/main/java/com/micael/collecta/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.micael.collecta.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundGray = Color(0xFFFAFAFA) // Cor #FAFAFA do PDF [cite: 1, 2]
+val ContainerGray = Color(0xFFD9D9D9)  // Cor #D9D9D9 dos mini containers [cite: 3]
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
